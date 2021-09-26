1809fa065b444c1264b7918b127924c65653aef3
bnechyporenko <bogdannechiporenko@gmail.com>
2021-09-26 14:33:26 +0200

    Changed web server from Tomcat to Netty

--- webshop-server/build.gradle.kts
+++ webshop-server/build.gradle.kts
@@ -27,8 +27,9 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-reactor-netty")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.hibernate:hibernate-core")
 
